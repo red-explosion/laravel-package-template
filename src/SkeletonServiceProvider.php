8837a78ad3bf1f8024f89a94c2a1418c879776c4
Ben Sherred <ben@sherred.co.uk>
2023-05-17 18:51:50 +0100

    chore: remove final keyboard

--- src/SkeletonServiceProvider.php
+++ src/SkeletonServiceProvider.php
@@ -6,7 +6,7 @@ namespace RedExplosion\Skeleton;
 
 use Illuminate\Support\ServiceProvider;
 
-final class SkeletonServiceProvider extends ServiceProvider
+class SkeletonServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
