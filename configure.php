203501effcfe39cdcf965cf79c8f74627db59b2b
Ben Sherred <ben@sherred.co.uk>
2023-11-23 08:45:17 +0000

    refactor: configure script to use arguments

--- configure.php
+++ configure.php
@@ -1,31 +1,33 @@
 #!/usr/bin/env php
 <?php
 
-function ask(string $question, string $default = ''): string
+function packageName(string $subject): string
 {
-    $answer = readline($question.($default ? " ({$default})" : null).': ');
-
-    if (! $answer) {
-        return $default;
-    }
-
-    return $answer;
+    return ucwords(str_replace('-', ' ', $subject));
 }
 
-function confirm(string $question, bool $default = false): bool
+function titleCase(string $subject): string
 {
-    $answer = ask($question.' ('.($default ? 'Y/n' : 'y/N').')');
-
-    if (! $answer) {
-        return $default;
-    }
-
-    return mb_strtolower($answer) === 'y';
+    return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $subject)));
 }
 
-function writeln(string $line): void
+function removePrefix(string $prefix, string $content): string
 {
-    echo $line.PHP_EOL;
+    if (str_starts_with($content, $prefix)) {
+        return mb_substr($content, mb_strlen($prefix));
+    }
+
+    return $content;
+}
+
+function replaceForWindows(): array
+{
+    return preg_split('/\\r\\n|\\r|\\n/', run('dir /S /B * | findstr /v /i .git\ | findstr /v /i vendor | findstr /v /i '.basename(__FILE__).' | findstr /r /i /M /F:/ ":author_name :author_username author@domain.com :package_name :package_slug Skeleton skeleton :package_description"'));
+}
+
+function replaceForAllOtherOSes(): array
+{
+    return explode(PHP_EOL, run('grep -E -r -l -i ":author_name|:author_username|author@domain.com|:package_name|:package_slug|Skeleton|skeleton|:package_description" --exclude-dir=vendor ./* ./.github/* | grep -v '.basename(__FILE__)));
 }
 
 function run(string $command): string
@@ -33,17 +35,7 @@ function run(string $command): string
     return trim((string) shell_exec($command));
 }
 
-function slugify(string $subject): string
-{
-    return mb_strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $subject), '-'));
-}
-
-function title_case(string $subject): string
-{
-    return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $subject)));
-}
-
-function replace_in_file(string $file, array $replacements): void
+function replaceInFile(string $file, array $replacements): void
 {
     $contents = file_get_contents($file);
 
@@ -57,16 +49,12 @@ function replace_in_file(string $file, array $replacements): void
     );
 }
 
-function remove_prefix(string $prefix, string $content): string
+function determineSeparator(string $path): string
 {
-    if (str_starts_with($content, $prefix)) {
-        return mb_substr($content, mb_strlen($prefix));
-    }
-
-    return $content;
+    return str_replace('/', DIRECTORY_SEPARATOR, $path);
 }
 
-function remove_readme_paragraphs(string $file): void
+function removeReadmeParagraphs(string $file): void
 {
     $contents = file_get_contents($file);
 
@@ -76,59 +64,22 @@ function remove_readme_paragraphs(string $file): void
     );
 }
 
-function determineSeparator(string $path): string
-{
-    return str_replace('/', DIRECTORY_SEPARATOR, $path);
-}
+$authorName = $argv[1];
+$authorEmail = $argv[2];
+$authorUsername = $argv[3];
+$packageName = packageName($argv[4]);
+$packageSlug = $argv[4];
+$packageSlugWithoutPrefix = removePrefix(prefix: 'laravel-', content: $packageSlug);
+$className = titleCase($packageSlug);
+$description = $argv[5] !== '' ? $argv[5] : 'A skeleton for building Red Explosion Laravel packages.';
 
-function replaceForWindows(): array
-{
-    return preg_split('/\\r\\n|\\r|\\n/', run('dir /S /B * | findstr /v /i .git\ | findstr /v /i vendor | findstr /v /i '.basename(__FILE__).' | findstr /r /i /M /F:/ ":author_name :author_username author@domain.com :package_name :package_slug Skeleton skeleton :package_description"'));
-}
-
-function replaceForAllOtherOSes(): array
-{
-    return explode(PHP_EOL, run('grep -E -r -l -i ":author_name|:author_username|author@domain.com|:package_name|:package_slug|Skeleton|skeleton|:package_description" --exclude-dir=vendor ./* ./.github/* | grep -v '.basename(__FILE__)));
-}
-
-$gitName = run('git config user.name');
-$authorName = ask('Author name', $gitName);
-
-$gitEmail = run('git config user.email');
-$authorEmail = ask('Author email', $gitEmail);
-
-$usernameGuess = explode(':', run('git config remote.origin.url'))[1];
-$usernameGuess = dirname($usernameGuess);
-$usernameGuess = basename($usernameGuess);
-$authorUsername = ask('Author username', $usernameGuess);
-
-$currentDirectory = getcwd();
+$currentDirectory = (string) getcwd();
 $folderName = basename($currentDirectory);
-
-$packageName = ask('Package name', $folderName);
-$packageSlug = slugify($packageName);
-$packageSlugWithoutPrefix = remove_prefix('laravel-', $packageSlug);
-
-$className = title_case($packageName);
-$className = ask('Class name', $className);
-$description = ask('Package description', "This is my package {$packageSlug}");
-
-writeln('------');
-writeln("Author     : {$authorName} ({$authorUsername}, {$authorEmail})");
-writeln("Package    : {$packageSlug} <{$description}>");
-writeln("Class name : {$className}");
-writeln('------');
-
-writeln('This script will replace the above values in all relevant files in the project directory.');
-
-if (! confirm('Modify files?', true)) {
-    exit(1);
-}
 
 $files = (str_starts_with(mb_strtoupper(PHP_OS), 'WIN') ? replaceForWindows() : replaceForAllOtherOSes());
 
 foreach ($files as $file) {
-    replace_in_file($file, [
+    replaceInFile($file, [
         ':author_name' => $authorName,
         ':author_username' => $authorUsername,
         'author@domain.com' => $authorEmail,
@@ -140,15 +91,10 @@ foreach ($files as $file) {
     ]);
 
     match (true) {
-        str_contains($file, determineSeparator('src/Skeleton.php')) => rename($file, determineSeparator('./src/'.$className.'.php')),
-        str_contains($file, determineSeparator('src/SkeletonServiceProvider.php')) => rename($file, determineSeparator('./src/'.$className.'ServiceProvider.php')),
-        str_contains($file, determineSeparator('config/skeleton.php')) => rename($file, determineSeparator('./config/'.$packageSlugWithoutPrefix.'.php')),
-        str_contains($file, 'README.md') => remove_readme_paragraphs($file),
+        str_contains($file, determineSeparator(path: 'src/Skeleton.php')) => rename($file, determineSeparator(path: './src/'.$className.'.php')),
+        str_contains($file, determineSeparator(path: 'src/SkeletonServiceProvider.php')) => rename($file, determineSeparator(path: './src/'.$className.'ServiceProvider.php')),
+        str_contains($file, determineSeparator(path: 'config/skeleton.php')) => rename($file, determineSeparator(path: './config/'.$packageSlugWithoutPrefix.'.php')),
+        str_contains($file, 'README.md') => removeReadmeParagraphs($file),
         default => [],
     };
 }
-
-
-confirm('Execute `composer install` and run tests?') && run('composer install && composer test');
-
-confirm('Let this script delete itself?', true) && unlink(__FILE__);
